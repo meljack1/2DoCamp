28c6b926a2d4842d36408e40c80f7d7f5a3949cb
Mel Jack <melissa_jack@hotmail.co.uk>
2021-09-14 21:50:07 +0100

    Add models and database structure

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Todo = require('./Todo');
+
+User.hasOne(Todo, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Todo.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Todo };
