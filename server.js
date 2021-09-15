c4bb47f5c8109371923ed653ffabf7c49ffd2fd5
Mel Jack <melissa_jack@hotmail.co.uk>
2021-09-15 17:08:07 +0100

    Fix glitch where sequelize session was not being created

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 
-const dbConnection = require('./config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -15,7 +15,7 @@ const sess = {
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: dbConnection
+      db: sequelize
     })
 };
 
@@ -27,6 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+  });
