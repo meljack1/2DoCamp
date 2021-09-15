5f5dd126d242ec63061d954a2665acf6b61dd26b
Mel Jack <melissa_jack@hotmail.co.uk>
2021-09-15 16:50:23 +0100

    Update server with sequelize session

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
-// Dependencies
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+//const routes = require('./controllers');
 
+const dbConnection = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'CDM best group!',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: dbConnection
+    })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
