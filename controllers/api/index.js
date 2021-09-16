a19507d95ab31e57dc53ac0cd5ee30bd0567f170
dylson <dylsonuni@gmail.com>
2021-09-16 21:47:46 +0100

    created the post request for toDo.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require("./userRoutes");
+const toDoRoutes = require("./toDoRoutes");
 
 router.use("/users", userRoutes);
+router.use("/todo", toDoRoutes);
 
 module.exports = router;
