f026925eda6db56cf46d0d011dc89310197668e8
dylson <dylsonuni@gmail.com>
2021-09-19 20:19:45 +0100

    brought in express handlebars, created HomeRoutes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -20,6 +21,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
