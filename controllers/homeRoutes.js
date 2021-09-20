40420619d89d8a33121e99f055f0daf91d0f0392
Dylson14 <83713974+Dylson14@users.noreply.github.com>
2021-09-20 18:10:49 +0100

    Ticket#19   convert UI to handlebars (#20)
    * created layouts folder and move handlebars in main
    
    * created homepage handlebars, passed inbody in main
    
    * brought in express handlebars, created HomeRoutes
    
    * corrected link to stylesheet. completed ui

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', function (req, res) {
+    res.render('homepage');
+});
+
+module.exports = router;
