f01ee8b5384eb4919cb826a5d2c52e63c4b4fec5
dylson <dylsonuni@gmail.com>
2021-09-21 18:23:28 +0100

    created GETroute to retrieve all the created todos

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Todo, User } = require('../models');
+const { rawAttributes } = require('../models/User');
 const withAuth = require('../utils/auth');
 
 router.get('/', function (req, res) {
     res.render('homepage');
 });
+
+router.get("/", async, (req, res) => {
+    try {/* Gathering all the to do notes from the user data */
+        const toDoData = await Todo.findAll()
+   
+
+
+/*Below serializes data so the template can read it.*/
+const toDos = toDoData.map((toDo) => toDo.get({ plain: true }));
+
+res.render("homepage", {
+toDos,
+logged_in: req.session.logged_in
+});
+
+} catch (error) {
+    res.status(500).json(error);
+}
+
+})
 
 module.exports = router;
