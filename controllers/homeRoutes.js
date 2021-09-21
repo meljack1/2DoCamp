e716495433071787f8050b30515c32923fa14653
dylson <dylsonuni@gmail.com>
2021-09-21 19:03:19 +0100

    updated homeRoutes by deleting unused code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,10 @@
 const router = require('express').Router();
-const { Todo, User } = require('../models');
-const { rawAttributes } = require('../models/User');
-const withAuth = require('../utils/auth');
+const { Todo } = require('../models');
 
-router.get('/', function (req, res) {
-    res.render('homepage');
-});
-
-router.get("/", async, (req, res) => {
+router.get("/", async (req, res) => {
     try {/* Gathering all the to do notes from the user data */
         const toDoData = await Todo.findAll()
    
-
-
 /*Below serializes data so the template can read it.*/
 const toDos = toDoData.map((toDo) => toDo.get({ plain: true }));
 
