5ea13c24ba7416a53648af999ca92b99be27a5af
Mel Jack <melissa_jack@hotmail.co.uk>
2021-09-23 21:52:59 +0100

    Only show tasks created by logged in user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,19 +3,23 @@ const { Todo } = require('../models');
 
 router.get("/", async (req, res) => {
     try {/* Gathering all the to do notes from the user data */
-        const toDoData = await Todo.findAll()
+        const toDoData = await Todo.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        })
    
-/*Below serializes data so the template can read it.*/
-const toDos = toDoData.map((toDo) => toDo.get({ plain: true }));
+        /*Below serializes data so the template can read it.*/
+        const toDos = toDoData.map((toDo) => toDo.get({ plain: true }));
 
-res.render("homepage", {
-toDos,
-logged_in: req.session.logged_in
-});
+        res.render("homepage", {
+        toDos,
+        logged_in: req.session.logged_in
+        });
 
-} catch (error) {
-    res.status(500).json(error);
-}
+    } catch (error) {
+        res.status(500).json(error);
+    }
 
 })
 
